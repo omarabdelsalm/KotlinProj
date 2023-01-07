8a0864d45df3c664ef3a53cc085d84231ab6e2c5
omarabdelsalm <93231975za@gmail.com>
2023-01-07 21:18:42 +0200

    add to fragmentkotlin

--- MyFragmentKotlin/app/src/main/java/com/example/omar/myfragmentkotlin/MainActivity.kt
+++ MyFragmentKotlin/app/src/main/java/com/example/omar/myfragmentkotlin/MainActivity.kt
@@ -2,14 +2,24 @@ package com.example.omar.myfragmentkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     val fragmentone = oneFragment()
+    val fragmenttow= fragmentTow()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getFragment(fragmentone)
+        var btn1=findViewById<Button>(R.id.btn1)
+        var btn2=findViewById<Button>(R.id.btn2)
+        btn1.setOnClickListener {
+            getFragment(fragmentone)
+        }
+        btn2.setOnClickListener {
+            getFragment(fragmenttow)
+        }
+
     }
     private fun getFragment(fragment: Fragment){
       val transaction =  supportFragmentManager.beginTransaction()
