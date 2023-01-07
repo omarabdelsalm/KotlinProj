6c573870b4d7d24b0f240cc90cdedd1784f6ee93
omarabdelsalm <93231975za@gmail.com>
2023-01-07 20:58:43 +0200

    New adding

--- MyFragmentKotlin/app/src/main/java/com/example/omar/myfragmentkotlin/MainActivity.kt
+++ MyFragmentKotlin/app/src/main/java/com/example/omar/myfragmentkotlin/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.omar.myfragmentkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    val fragmentone = oneFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getFragment(fragmentone)
+    }
+    private fun getFragment(fragment: Fragment){
+      val transaction =  supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frgone1,fragment)
+        transaction.commit()
+
     }
 }
